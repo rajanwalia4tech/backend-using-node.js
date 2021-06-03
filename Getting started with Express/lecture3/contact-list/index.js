3f6b60bfde62fcfb11209df8ba0c94bdfb716421
rajan <rajanwalia4@gmail.com>
2021-06-03 15:27:40 +0530

    changes in about.ejs and index.js

--- Getting started with Express/lecture3/contact-list/index.js
+++ Getting started with Express/lecture3/contact-list/index.js
@@ -8,13 +8,18 @@ app.set('view engine','ejs');
 //app.set('views','views');
 app.set('views',path.join(__dirname,'views'))
 app.get('/', (req, res) => {
-	
 	res.render("home",{title:"Home"})
-	
   //res.send(`Hello World!`)
-  
+})
+
+app.get("/about",(req,res)=>{
+  res.render("about",{
+    title:"about"
+  })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// Install : npm install express ejs
