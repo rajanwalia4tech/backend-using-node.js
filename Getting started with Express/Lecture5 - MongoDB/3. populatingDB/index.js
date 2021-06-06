ae49f1b15266c86e77c271d0444f593871133f68
rajan <rajanwalia4@gmail.com>
2021-06-06 23:18:50 +0530

    Getting started with MongoDB

---
+++ Getting started with Express/Lecture5 - MongoDB/3. populatingDB/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const port = 8000;
+
+const db = require('./config/mongoose');
+const Contact = require('./models/contact');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+
+
+var contactList = [
+    {
+        name: "Arpan",
+        phone: "1111111111"
+    },
+    {
+        name: "Tony Stark",
+        phone: "1234567890"
+    },
+    {
+        name: "Coding Ninjas",
+        phone: "12131321321"
+    }
+]
+
+app.get('/practice', function(req, res){
+    return res.render('practice', {
+        title: "Let us play with ejs"
+    });
+});
+
+
+app.get('/', function(req, res){
+
+    return res.render('home',{
+        title: "Contact List",
+        contact_list: contactList
+    });
+})
+app.post('/create-contact', function(req, res){
+    
+    
+    Contact.create({
+        name: req.body.name,
+        phone: req.body.name
+    }, function(err, newContact){
+        if(err){console.log('Error in creating a contact!')
+            return;}
+            console.log('******', newContact);
+            return res.redirect('back');
+    })
+  
+
+});
+
+app.listen(port, function(err){
+    if (err) {
+        console.log("Error in running the server", err);
+    }
+    console.log('Yup!My Server is running on Port', port);
+})
+
+
+app.get('/delete-contact/', function(req, res){
+    console.log(req.query);
+    let phone = req.query.phone
+
+    let contactindex = contactList.findIndex(contact => contact.phone == phone);
+
+    if(contactindex != -1){
+        contactList.splice(contactindex, 1);
+    }
+
+    return res.redirect('back');
+});
