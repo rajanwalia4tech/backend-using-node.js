a3640052f4a2f4af5fa69fe3d36b3d3fafde8326
rajan <rajanwalia4@gmail.com>
2021-06-09 21:53:09 +0530

    Added Manual Authentication

---
+++ Getting started with Express/Lecture7 - Manual Authentication/2 - Render Signin-Signup Pages/codeial/controllers/users_controller.js
@@ -0,0 +1,21 @@
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
